72b723fac661f0229a80ca4e06abcc4c0aaddca5
Markus Mertama <mertama@gmail.com>
2018-12-30 17:52:51 +0200

    cleaning Axq.pri and fixing linuxdeploy.sh

--- releases/linuxdeploy.sh
+++ releases/linuxdeploy.sh
@@ -5,26 +5,32 @@ then
     ver=${BASH_REMATCH[1]}
     major=${BASH_REMATCH[2]}
     minor=${BASH_REMATCH[3]}
-    axqbuild="../../build-Axq-Desktop_Qt_${ver}_${major}_${minor}_GCC_64bit?-Release"
-    mdmbuild="../../build-mdmaker-Desktop_Qt_${ver}_${major}_${minor}_GCC_64bit?-Release"
+    # static build axqbuild=$(ls -d "../../build-Axq-Desktop_Qt_${ver}_${major}_${minor}_GCC_64bit"?"-Release")
+    mdmbuild=$(ls -d "../../build-mdmaker-Desktop_Qt_${ver}_${major}_${minor}_GCC_64bit"?"-Release")
+    
+    if [[ ! -d $mdmbuild ]] ; then
+        echo Error: $mdmbuild not found >&2
+        exit 1
+    fi
     
     export QT_SELECT=qt${ver}.${major}.${minor}
     
-    cp "${axqbuild}/lib/libAxq.so.1"  "MarkdownMaker-x86_64.AppDir/usr/lib/"
-    cp "${mdmbuild}/mdmaker"          "MarkdownMaker-x86_64.AppDir/usr/bin/"
+    # static build cp ${axqbuild}/lib/libAxq.so.1  MarkdownMaker-x86_64.AppDir/usr/lib/
+    cp ${mdmbuild}/mdmaker          MarkdownMaker-x86_64.AppDir/usr/bin/
     
     if [[ ! -f MarkdownMaker-x86_64.AppDir/usr/lib/libsoftokn3.so ]] ; then
      # ln -s /usr/lib/x86_64-linux-gnu/nss/libsoftokn3.so  MarkdownMaker-x86_64.AppDir/usr/lib/libsoftokn3.so
 	    cp -s /usr/lib/x86_64-linux-gnu/nss/libsoftokn3.so  MarkdownMaker-x86_64.AppDir/usr/lib/libsoftokn3.so
         if [[ ! -f MarkdownMaker-x86_64.AppDir/usr/lib/libsoftokn3.so ]] ; then
-            echo Warning MarkdownMaker-x86_64.AppDir/usr/lib/libsoftokn3.so does not exists, that may be an issue
+            echo Warning: MarkdownMaker-x86_64.AppDir/usr/lib/libsoftokn3.so does not exists, that may be an issue >&2
         fi
     fi      
 
     linuxdeployqt-continuous-x86_64.AppImage MarkdownMaker-x86_64.AppDir/usr/share/applications/MarkdownMaker.desktop -appimage -always-overwrite -qmldir=.. -extra-plugins=webview
   
 else
-    echo Error, QTDIR is not set
+    echo Error: QTDIR is not set >&2
+    exit
 fi
 
 
